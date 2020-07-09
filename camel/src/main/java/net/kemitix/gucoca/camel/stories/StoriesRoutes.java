f8dcdbe3eb1a268a8b7240e9a7d2371dcb528aa6
Paul Campbell <pcampbell@kemitix.net>
2020-07-09 18:48:45 +0100

    Use velocity template for notification email (#62)

--- camel/src/main/java/net/kemitix/gucoca/camel/stories/StoriesRoutes.java
+++ camel/src/main/java/net/kemitix/gucoca/camel/stories/StoriesRoutes.java
@@ -2,13 +2,15 @@ package net.kemitix.gucoca.camel.stories;
 
 import net.kemitix.gucoca.camel.email.SendEmail;
 import net.kemitix.gucoca.spi.GucocaConfig;
-import net.kemitix.gucoca.spi.Story;
-import org.apache.camel.Message;
-import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.SimpleBuilder;
+import org.apache.camel.builder.ValueBuilder;
+import org.apache.camel.component.aws.ses.SesConstants;
 
 import javax.inject.Inject;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Collections;
 
 class StoriesRoutes
         extends RouteBuilder
@@ -19,7 +21,7 @@ class StoriesRoutes
     @Inject StoryLoader storyLoader;
 
     @Override
-    public void configure() {
+    public void configure() throws UnknownHostException {
 
         from(LOAD_STORIES)
                 .routeId("load-stories")
@@ -35,31 +37,22 @@ class StoriesRoutes
                 .bean(storyLoader, "addStoryCard")
         ;
 
+        ValueBuilder sender = constant(config.getEmailSender());
+        ValueBuilder recipient = constant(Collections.singletonList(
+                config.getNotificationRecipient()));
+        SimpleBuilder subject = simple(
+                "Story Selected: ${body.title} by ${body.author}");
+        ValueBuilder hostname = constant(
+                InetAddress.getLocalHost().getHostName());
         from(NOTIFY_SELECTION)
-                .process(prepareNotificationEmail())
+                .setHeader(SesConstants.FROM, sender)
+                .setHeader(SesConstants.TO, recipient)
+                .setHeader(SesConstants.HTML_EMAIL, constant(true))
+                .setHeader(SesConstants.SUBJECT, subject)
+                .setHeader("Hostname", hostname)
+                .to("velocity://gucoca/stories/selected-notice.vm")
                 .to(SendEmail.SEND)
         ;
     }
 
-    private Processor prepareNotificationEmail() {
-        return exchange -> {
-            Message in = exchange.getIn();
-            Story story = in.getBody(Story.class);
-            sendEmail
-                    .message(in)
-                    .from(config.getEmailSender())
-                    .to(config.getNotificationRecipient())
-                    .subject(String.format("Story Selected: %s by %s",
-                            story.getTitle(),
-                            story.getAuthor()
-                    ))
-                    .html()
-                    .body(String.format("<h1>Selected: %s by %s</h1>" +
-                                    "<p>This story has been selected to be promoted " +
-                                    "on Twitter.</p>" +
-                                    "<p>Sent from: %s</p>",
-                            story.getTitle(), story.getAuthor(),
-                            InetAddress.getLocalHost().getHostName()));
-        };
-    }
 }
